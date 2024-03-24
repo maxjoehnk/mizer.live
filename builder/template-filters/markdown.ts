644273f84ee2bd139a25f42116822b39b95ffc62
Max Jöhnk <maxjoehnk@gmail.com>
2024-03-24 01:59:53 +0100

    fix(template-filter): remove highlight dependency

--- builder/template-filters/markdown.ts
+++ builder/template-filters/markdown.ts
@@ -1,12 +1,3 @@
 import * as marked from 'marked';
-import hljs from 'highlight.js';
-
-marked.setOptions({
-  highlight(code: string, lang: string): string | void {
-    const language = hljs.getLanguage(lang) ? lang : 'plaintext'
-
-    return hljs.highlight(code, { language }).value
-  }
-})
 
 export default marked;
